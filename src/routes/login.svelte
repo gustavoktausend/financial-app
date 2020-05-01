99169e808dec1db90493198245379ac9123fe4e0
Gustavo Kring Tausendfreund <gustavoktausend@gmail.com>
2020-05-01 15:30:00 -0300

    tela de login primeiro commit

---
+++ src/routes/login.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Paper from '@smui/paper';
+    import Textfield from '@smui/textfield';
+    import Fab from '@smui/fab';
+    import FormField from '@smui/form-field';
+
+    export let email = '';
+    export let password = '';
+</script>
+
+<svelte:head>
+    <title>Login de Acesso</title>
+</svelte:head>
+
+<br/>
+<h1>Acesso a Plataforma</h1>
+<br/>
+<Paper>
+    <FormField>
+        <Textfield class="shaped-outlined" label="Email" bind:value={email} type="email" input$autocomplete="email" />
+    </FormField>
+    <br/>
+    <FormField>
+        <Textfield class="shaped-outlined" label="Password" bind:value={password} type="password" />
+    </FormField>
+    <br/>
+    <Fab on:click={()=> alert('login') } color="primary" extended >Login</Fab>
+</Paper>
